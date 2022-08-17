58f9066250f55afd5c6804cf900eefea679256d1
Sophia Kenneded <S>
2022-08-17 11:33:22 -0400

    Initial Commit

---
+++ codette.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  codette.2
+//
+//  Created by Sophia Kenneded on 8/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
